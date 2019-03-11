d819a6c2a1e44d6997ba997f5fe3d1daf197586f
pc-michi <pc-michi@172.21.57.5>
2019-03-11 13:52:33 +0100

    Metodi standard

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,38 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
 public class Voto {
+	
+	private int voto;
+	private String coso;
+	private LocalDate data;
+	
+	public Voto(int voto, String coso, LocalDate data) {
+		
+		this.voto = voto;
+		this.coso = coso;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCoso() {
+		return coso;
+	}
+	public void setCoso(String coso) {
+		this.coso = coso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
 
 }
